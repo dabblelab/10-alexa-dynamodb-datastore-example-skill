a5abd854a565f3e59d2fac55687ec1a532b6c668
Steve Tingiris <steve@dabblelab.com>
2018-05-27 09:18:09 -0400

    updates for ask-sdk 2.0

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,113 +1,104 @@
-'use strict';
-var Alexa = require("alexa-sdk");
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
 
-exports.handler = function(event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hello there. What is your name?';
+    const repromptText = 'Can you tell me your name?';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .withSimpleCard('Example Card Title', "Example card body content.")
+      .getResponse();
+  },
 };
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
-    'MyNameIsIntent': function () {
-        this.emit('SayHelloName');
-    },
-    'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
-        this.emit(':responseReady');
-    },
-    'SayHelloName': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
-    },
-    'AMAZON.StopIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'Unhandled': function () {
-        //log the event sent by the Alexa Service in human readable format
-        console.log(JSON.stringify(this.event));
-        let skillId, requestType, dialogState, intent ,intentName, intentConfirmationStatus, slotArray, slots, count;
+const MyNameIsIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'MyNameIsIntent';
+  },
+  handle(handlerInput) {
 
-        try {
-            //Parse necessary data from JSON object using dot notation
-            //build output strings and check for undefined
-            skillId = this.event.session.application.applicationId;
-            requestType = "This is an Unhandled request. The request type is, "+this.event.request.type+" .";
-            dialogState = this.event.request.dialogState;
-            intent = this.event.request.intent;
-            if (intent != undefined) {
-                intentName = " The intent name is, "+this.event.request.intent.name+" .";
-                slotArray = this.event.request.intent.slots;
-                intentConfirmationStatus = this.event.request.intent.confirmationStatus;
+    const nameSlot = handlerInput.requestEnvelope.request.intent.slots.name.value;
+    const speechText = `Hello ${nameSlot}. It's nice to meet you.`;
 
-                if (intentConfirmationStatus != "NONE" && intentConfirmationStatus != undefined ) {
-                    intentConfirmationStatus = " and its confirmation status is "+ intentConfirmationStatus+" . ";
-                    intentName = intentName+intentConfirmationStatus;
-                }
-            } else {
-                intentName = "";
-                slotArray = "";
-                intentConfirmationStatus = "";
-            }
-
-            slots = "";
-            count = 0;
-
-            if (slotArray == undefined || slots == undefined) {
-                slots = "";
-            }
-
-            //Iterating through slot array
-            for (let slot in slotArray) {
-                count += 1;
-                let slotName = slotArray[slot].name;
-                let slotValue = slotArray[slot].value;
-                let slotConfirmationStatus = slotArray[slot].confirmationStatus;
-                slots = slots + "The <say-as interpret-as='ordinal'>"+count+"</say-as> slot is, " + slotName + ", its value is, " +slotValue;
-
-                if (slotConfirmationStatus!= undefined && slotConfirmationStatus != "NONE") {
-                  slots = slots+" and its confirmation status is "+slotConfirmationStatus+" . ";
-                } else {
-                  slots = slots+" . ";
-                }
-            }
-
-            //Delegate to Dialog Manager when needed
-            //<reference to docs>
-            if (dialogState == "STARTED" || dialogState == "IN_PROGRESS") {
-              this.emit(":delegate");
-            }
-        } catch(err) {
-            console.log("Error: " + err.message);
-        }
-
-        let speechOutput = "Your end point receive a request, here's a breakdown. " + requestType + " " + intentName + slots;
-        let cardTitle = "Skill ID: " + skillId;
-        let cardContent = speechOutput;
-
-        this.response.cardRenderer(cardTitle, cardContent);
-        this.response.speak(speechOutput);
-        this.emit(':responseReady');
-      }
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
 };
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can introduce yourself by telling me your name';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    MyNameIsIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Hello World Alexa template
 
-This is a simple template that can be used as a starting point for building a simple Alexa skill.
+This is a simple template that can be used as a starting point for building a simple Alexa skill. This template uses the [Alexa Skills Kit for Node.js](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs) version 2.0 and is designed to be used with the [Alexa Skills Kit CLI](https://developer.amazon.com/docs/smapi/ask-cli-intro.html).
